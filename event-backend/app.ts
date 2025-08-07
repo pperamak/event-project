12f6597a7488215bc94edaf4dfcdb9bbc8bf9025
pperamak <pperamak@gmail.com>
2025-08-07 14:38:57 +0300

    set up docker dev db and test connection from be

--- event-backend/app.ts
+++ event-backend/app.ts
@@ -1,7 +1,25 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
+import dotenv from 'dotenv';
+dotenv.config();
 import express from 'express';
 import userRouter from './routes/users';
+import { Sequelize } from 'sequelize';
 const app = express();
 app.use(express.json());
+
+const sequelize = new Sequelize(process.env.DATABASE_URL!);
+
+const main = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+    sequelize.close();
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+};
+
+main();
 
 app.get('/ping', (_req, res) => {
   console.log('someone pinged here');
