9c4c62e91bdb773c4d511ab8527816e2b409a139
pperamak <pperamak@gmail.com>
2025-08-20 16:52:19 +0300

    set up createUser mutation

--- event-backend/src/graphql/resolvers.ts
+++ event-backend/src/graphql/resolvers.ts
@@ -1,5 +1,11 @@
 import User  from '../models/user';
+import bcrypt from 'bcrypt';
 
+interface CreateUserArgs {
+  name: string;
+  email: string;
+  password: string;
+}
 
 const resolvers = {
   Query: {
@@ -7,7 +13,43 @@ const resolvers = {
      const users = await User.findAll();
      return users.map((user) => user.toJSON());;
     }
+  },
+  Mutation: {
+    createUser: async (_root: unknown, args: CreateUserArgs) =>{
+      const { name, email, password } =args;
+      const saltRounds = 10;
+      const passwordHash = await bcrypt.hash(password, saltRounds);
+      const newUser = {
+        name,
+        email,
+        passwordHash
+        };
+      const savedUser = await User.create(newUser);
+
+      // hide passwordHash before returning
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { passwordHash: _, ...safeUser } = savedUser.toJSON();
+
+      return safeUser;
+      
+    }
   }
 };
+/*
+const { name, email, password } = req.body;
+  const saltRounds = 10;
+  const passwordHash = await bcrypt.hash(password, saltRounds);
+  console.log(name, email, passwordHash);
+  const newUser = {
+    name,
+    email,
+    passwordHash
+  };
+
+   
+  const savedUser = await User.create(newUser);
+  console.log(savedUser);
+  return res.send(savedUser.toJSON());
+  */
 
 export default resolvers;
--- event-backend/src/graphql/typeDefs.ts
+++ event-backend/src/graphql/typeDefs.ts
@@ -1,13 +1,22 @@
-export const typeDefs=`
+import { gql } from 'graphql-tag';
+
+export const typeDefs=gql`
   type User {
     name: String!
     email: String!
-    passwordHash: String!
     id: ID!
   }
   
   type Query {
     allUsers: [User!]!
+  }
+
+  type Mutation {
+    createUser(
+      name: String!
+      email: String!
+      password: String!
+    ): User!
   }
 `;
 
