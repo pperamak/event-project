8bf3db4a5b8d971d73a06b25c958f14e1c77c0cf
pperamak <pperamak@gmail.com>
2025-08-22 12:14:43 +0300

    fixed frontend test

--- event-frontend/src/App.test.tsx
+++ event-frontend/src/App.test.tsx
@@ -1,9 +1,14 @@
 import { render, screen } from '@testing-library/react';
 //import { test, expect } from 'vitest'
+import { MockedProvider } from '@apollo/client/testing';
 import App from './App';
 
 test('renders headline', () => {
-  render(<App />);
+  render(
+  <MockedProvider mocks={[]} addTypename={false}>
+    <App />
+  </MockedProvider>
+  );
   expect(screen.getByText(/Something/i)).toBeInTheDocument();
 });
 
